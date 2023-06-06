8c4d580e6ca623ab0e48cd02840df6b2d8b4984c
OrzeFox <dacm1997@yahoo.com>
2023-06-06 21:25:11 +0200

    nuevos contenidos

---
+++ dump/dificulty_&_category.sql
@@ -0,0 +1,3 @@
+INSERT INTO dificulty (`id`, `dificulty_level`) VALUES (1, 'easy'), (2, 'medium'), (3, 'hard');
+
+INSERT INTO categorie (id, categorie_name) VALUES (1, 'Programación'), (2, 'Diseño UX'), (3, 'Diseño Gráfico'), (4, 'Desarrollo Web'), (5, 'Inteligencia Artificial'); 
--- dump/modelos_de_consultas.sql
+++ dump/modelos_de_consultas.sql
@@ -1,4 +1,13 @@
-use mydb;
+-- SELECT content.title,  categorie.categorie_name
+-- FROM content
+-- LEFT JOIN categorie ON content.categorie_id = categorie.id;
 
-select id, title, price, dificulty from content order by () ;
-select * from content;
+-- SELECT c.title, ca.categorie_name
+-- FROM `content` AS c
+-- INNER JOIN `categorie` AS ca ON c.categorie_id = ca.id;
+
+-- SELECT c.title, ca.dificulty_level
+-- FROM `content` AS c
+-- INNER JOIN `dificulty` AS ca ON c.dificulty_id = ca.id;
+
+SELECT title, description FROM content WHERE price > 150;
